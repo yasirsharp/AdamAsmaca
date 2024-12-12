9a6bf9a9db2e96bf3d097f36c432fa406f8af7b6
yasirsharp <yasirsharp@hotmail.com>
2024-12-12 11:29:07 +0300

    Add project files.

---
+++ AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::AdamAsmaca.Properties.Resources.kolsag;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(449, 204);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 72);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::AdamAsmaca.Properties.Resources.Kol;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(352, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 76);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::AdamAsmaca.Properties.Resources.Vucut;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(347, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 126);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::AdamAsmaca.Properties.Resources.Kafa;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(347, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 158);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::AdamAsmaca.Properties.Resources.idamIpi;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(69, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(562, 465);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::AdamAsmaca.Properties.Resources.Kol;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(348, 319);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 76);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::AdamAsmaca.Properties.Resources.kolsag;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(446, 319);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 72);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 496);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1401, 158);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 42);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(690, 48);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(161, 152);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Harfi Tahmin Et";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(10, 93);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(145, 53);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Tahmin Et";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(797, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1485, 666);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string tahminKelime = "muvaffakiyetsizleştirebileceklerimiştenmişsizcesine";
+
+        char[] tahminEdilen = new char[tahminKelime.Length];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+
+            for (int i = 0; i < tahminKelime.Length; i++)
+            {
+                tahminEdilen[i] = '_';
+            }
+
+        }
+
+        int sayac = 0;
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int sayac1 = 0;
+            label1.Text = "";
+            label2.Text = "";
+
+            if (tahminKelime.IndexOf(textBox1.Text[0]) >= 0)
+            {
+                foreach (var item in tahminKelime)
+                {
+                    if (textBox1.Text[0] == item)
+                    {
+                        tahminEdilen[sayac1] = item;
+                    }
+                    sayac1++;
+                }
+                foreach (var item in tahminEdilen)
+                {
+                    label1.Text += item + " ";
+                    label2.Text += item;
+                }
+            }
+            else
+            {
+
+                AdamAs(sayac);
+                sayac++;
+            }
+
+            label2.Text = label2.Text.Trim(' ');
+            if (label2.Text == tahminKelime)
+            {
+                DialogResult = MessageBox.Show("Kazandınız");
+                if (DialogResult == DialogResult.OK) this.Close();
+            }
+            textBox1.Text = "";
+        }
+
+        private void AdamAs(int sayac)
+        {
+            foreach (var item in tahminEdilen)
+            {
+                label1.Text += item + " ";
+            }
+            switch (sayac)
+            {
+                case 0:
+                    button2.Visible = true; break;
+                case 1:
+                    button3.Visible = true; break;
+                case 2:
+                    button4.Visible = true; break;
+                case 3:
+                    button5.Visible = true; break;
+                case 4:
+                    button6.Visible = true; break;
+                case 5:
+                    button7.Visible = true; break;
+                default:
+                    GameOver();
+                    break;
+            }
+        }
+
+        private void GameOver()
+        {
+            DialogResult result = MessageBox.Show($"{tahminKelime}\nOyun Bitti", "Yandınız", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+
+                Form1 form1 = new Form1();
+                this.Close();
+                form1.Show();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+    }
+}
